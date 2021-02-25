9282cd5769dc16673d7ff45c0cdbbc8eaf550247
mgxmajewski <60788689+mgxmajewski@users.noreply.github.com>
2021-02-25 14:55:42 +0100

    Add isWordGuessed() logic

--- ps3_hangman.py
+++ ps3_hangman.py
@@ -44,14 +44,16 @@ def chooseWord(wordlist):
 wordlist = loadWords()
 
 def isWordGuessed(secretWord, lettersGuessed):
-    '''
+    """
     secretWord: string, the word the user is guessing
     lettersGuessed: list, what letters have been guessed so far
     returns: boolean, True if all the letters of secretWord are in lettersGuessed;
       False otherwise
-    '''
-    # FILL IN YOUR CODE HERE...
-
+    """
+    for char in secretWord:
+        if char not in lettersGuessed:
+            return False
+    return True
 
 
 def getGuessedWord(secretWord, lettersGuessed):
